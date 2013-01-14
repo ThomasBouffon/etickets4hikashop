e13ef4f476b4a8a76ab4fc532c34c9cd97b50476
Thomas Bouffon <thomas.bouffon@gmail.com>
2013-01-14 07:07:44 +0100

    Everything now relies on hikashop triggers. No need for the system plugin anymore Added control on the ticket generation

--- plugins/hikashop/etickets/etickets.php
+++ plugins/hikashop/etickets/etickets.php
@@ -40,6 +40,23 @@ class plgHikashopEtickets extends JPlugin
 			$this->database->query();
 		}
 	}
+	function getTicketListForOrder($order_id) {
+		$query = 'select id FROM '.hikashop_table('etickets').' WHERE order_id='.$order_id;
+		$this->database->setQuery($query);
+		$ret=array();
+		$result=$this->database->loadObjectList();
+		error_log("xxxx".var_export($result,true));
+		if ($result!=null) {
+			foreach ($result as $row) {
+			error_log("id=".$row->id);
+			array_push($ret,$row->id);
+			}
+		}
+		return $ret;
+	}
+
+
+
 	function onAfterOrderCreate(&$order,&$send_email){
 		return $this->onAfterOrderUpdate($order,$send_email);
 	}
@@ -158,5 +175,99 @@ class plgHikashopEtickets extends JPlugin
 			}
 
 		}
+		return true;
 	}
+	function onBeforeMailSend(&$mail, &$mailer) {
+		$order_id=$mail->data->order_id;
+		if (! $order_id) {return true;}
+		$ticketList=$this->getTicketListForOrder($order_id);
+		error_log(var_export($ticketList,true));
+		foreach($ticketList as $ticketId) {
+			$attachObj=null;
+			$attachObj->filename="Ticket-".$ticketId.".pdf";
+			$attachObj->contentAsText=$this->createTicketFile($ticketId);
+			$mailer->AddStringAttachment($attachObj->contentAsText,$attachObj->filename);
+		}
+		return true;
+
+
+
+	}
+	function createTicketFile ($eTicketID) {
+		require_once(__DIR__.'/lib/tcpdf/config/lang/fra.php');
+		require_once(__DIR__.'/lib/tcpdf/tcpdf.php');
+		$pdf=new TCPDF('P', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+
+		error_log("Ticket : $eTicketID");
+		$db = JFactory::getDBO();
+		$query='SELECT a.*,order_product_name FROM '.hikashop_table('eticket_info').' AS a, '.hikashop_table('etickets').' AS b, '.hikashop_table('order_product').' AS c WHERE a.product_id=c.product_id AND c.order_product_id=b.order_product_id AND b.id=\''.$eTicketID."'";
+		$this->database->setQuery($query);
+		$info=$this->database->loadObjectList();
+		$info=$info[0];
+		$query='SELECT order_product_price FROM '.hikashop_table('etickets').' AS b, '.hikashop_table('order_product').' AS c WHERE c.order_product_id=b.order_product_id AND b.id=\''.$eTicketID."'";
+		$this->database->setQuery($query);
+		$price=$this->database->loadResult();
+		$query='SELECT tn FROM '.hikashop_table('etickets').' WHERE id=\''.$eTicketID."'";
+		$this->database->setQuery($query);
+		$tn=$this->database->loadResult();
+		$query = "SELECT config_value FROM #__hikashop_eticket_config WHERE config_key='codetype'";
+		$db->setQuery($query);
+		$codeType=$db->loadResult();
+		$view=new JView();
+		$view->addTemplatePath(__DIR__ . '/tmpl/');
+
+		$view->setLayout("eticket");
+		$html=$view->loadTemplate();
+		$html=str_replace('ET4H_ADDRESS',$info->address,$html);
+		$html=str_replace('ET4H_EVENTDATE',$info->eventdate,$html);
+		$html=str_replace('ET4H_PRICE',sprintf("%.2f",$price),$html);
+		$html=str_replace('ET4H_TN',$tn,$html);
+		$config =& JFactory::getConfig();
+		$html=str_replace('ET4H_SITE_NAME',$config->getValue("config.sitename"),$html);
+		$html=str_replace('ET4H_EVENT_NAME',$info->order_product_name,$html);
+		if ($codeType == "barcode") {
+			$tcpdfParams = $pdf->serializeTCPDFtagParameters(array($eTicketID, 'C128','','','120','30' )); 
+			$html=str_replace('ET4H_BARCODE','<tcpdf method="write1DBarcode" params="'.$tcpdfParams.'" />',$html);
+		}
+		else if ($codeType == "qrcode") {
+			$tcpdfParams = $pdf->serializeTCPDFtagParameters(array($eTicketID, 'QRCODE,B','','','50','50' )); 
+			$html=str_replace('ET4H_BARCODE','<tcpdf method="write2DBarcode" params="'.$tcpdfParams.'" />',$html);
+		}
+		$pdf->AddPage();
+
+		$pdf->writeHTML($html, true, false, true, false, '');
+
+
+		return $pdf->Output('',S);
+
+	}
+	function onAfterOrderProductsListingDisplay(&$order, $where) {
+		if ($where =='order_front_show') {
+			$order_id=$order->order_id;
+			if (! $order_id) {return true;}
+			$ticketList=$this->getTicketListForOrder($order_id);
+			$eTicketID=JRequest::getVar("eTicketID");
+			if ($eTicketID && in_array($eTicketID,$ticketList)) {
+				ob_clean();
+				header('Content-type: application/pdf');
+				header('Content-Disposition: attachment; filename="ETicket-'.$eTicketID.'.pdf"');
+				echo $this->createTicketFile($eTicketID);
+
+				exit;
+			}
+			error_log(var_export($ticketList,true));
+			if ($ticketList) {
+				echo "<p>This order contains electronic tickets :";
+				echo "<ul>";
+				foreach($ticketList as $key=>$ticketId) {
+					echo '<li><a target=blank href='.hikashop_completeLink('order&task=show&cid='.$order_id.'&eTicketID='.$ticketId).'>Ticket #'.$key.'</a></li>';
+				}
+				echo "<ul></p>";
+			}
+		}
+
+		
+		return true;
+	}
+
 }
